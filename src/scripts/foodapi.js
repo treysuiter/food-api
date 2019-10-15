8c611d58b0e4bd458f031317a5471aae89208447
Trey Suiter <trey.suiter@gmail.com>
2019-10-15 14:46:38 -0500

    created to functions to create html element and add to dom

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,12 +1,28 @@
 console.log("We good")
 
+function foodFactory (food) {
+    console.log(food)
+    return `
+    <div class="foodItem">
+        <h1>${food.name}</h1>
+        <p>${food.ethnicity}</p>
+        <p>${food.category}</p>
+    </div>
+    `
+}
+
+function addFoodToDom (foodHTML) {
+    const foodContainer = document.querySelector(".foodList")
+    foodContainer.innerHTML += foodHTML
+}
+
 fetch("http://localhost:8088/food")
     .then(foods => foods.json())
     .then(parsedFoods => {
-        console.log(parsedFoods)
         parsedFoods.forEach(food => {
-            console.log(food)
-            console.log(food.name)
+            const foodAsHTML = foodFactory(food)
+            addFoodToDom(foodAsHTML)
         })
     })
+
 
